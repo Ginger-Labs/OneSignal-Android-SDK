7ae4f87e329968068c83fd43443a325743ecd7ca
Mihail Gutan <Mihail@gingerlabs.com>
2020-01-06 15:46:31 -0800

    progress

--- OneSignalSDK/onesignal/src/main/java/com/onesignal/GenerateNotification.java
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/GenerateNotification.java
@@ -63,6 +63,7 @@ import android.service.notification.StatusBarNotification;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.text.SpannableString;
 import android.text.style.StyleSpan;
 import android.widget.RemoteViews;
@@ -1058,12 +1059,41 @@ class GenerateNotification {
             int buttonIcon = 0;
             if (button.has("icon"))
                buttonIcon = getResourceIcon(button.optString("icon"));
-            
-            mBuilder.addAction(buttonIcon, button.optString("text"), buttonPIntent);
+
+            String title = button.optString("text");
+
+            if (title.equalsIgnoreCase("reply")) {
+               // This portion overrides the regular reply button with the android specific one.
+               String replyLabel = "Enter your reply here";
+
+               // Initialise RemoteInput
+               RemoteInput remoteInput = new RemoteInput.Builder( "key_reply")
+                       .setLabel(replyLabel)
+                       .build();
+
+               Intent sendIntent = new Intent(currentContext, SendReceiver.class);
+               PendingIntent pendingIntent = PendingIntent.getBroadcast(currentContext, 0, sendIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+               // Notification Action with RemoteInput instance added.
+               NotificationCompat.Action replyAction = new NotificationCompat.Action.Builder(
+                       buttonIcon, title, pendingIntent)
+                       .addRemoteInput(remoteInput)
+                       .setAllowGeneratedReplies(true)
+                       .build();
+
+               // Notification.Action instance added to Notification Builder.
+               mBuilder.addAction(replyAction);
+            } else {
+               mBuilder.addAction(buttonIcon, title, buttonPIntent);
+            }
          }
       } catch (Throwable t) {
          t.printStackTrace();
       }
+   }
+
+   private static void overrideReplyButton(){
+
    }
 
    private static void addAlertButtons(Context context, JSONObject gcmBundle, List<String> buttonsLabels, List<String> buttonsIds) {
---
+++ OneSignalSDK/onesignal/src/main/java/com/onesignal/SendReceiver.java
@@ -0,0 +1,50 @@
+package com.onesignal;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.RemoteInput;
+import android.util.Log;
+
+public class SendReceiver extends BroadcastReceiver {
+
+    public static OnInstantReplyListener onInstantReplyListener;
+
+    interface OnInstantReplyListener {
+        void onReply(String replyText);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+
+        if (remoteInput != null) {
+            final CharSequence replySeq = remoteInput.getCharSequence("key_reply");
+            if (replySeq != null) {
+                if (onInstantReplyListener != null) {
+                    final PendingResult result = goAsync();
+                    Thread thread = new Thread() {
+                        public void run() {
+                            int i = 1234;
+                            onInstantReplyListener.onReply(replySeq.toString());
+                            // Do processing
+                            result.setResultCode(i);
+                            result.finish();
+                        }
+                    };
+                    thread.start();
+
+                } else {
+                    Log.e(this.getClass().getSimpleName(), "onReceive: Got instant reply but no onInstantReplyListener to fire.");
+                }
+            } else {
+                Log.e(this.getClass().getSimpleName(), "onReceive: Instant reply CharSequence is null");
+            }
+
+        }
+
+    }
+
+}
